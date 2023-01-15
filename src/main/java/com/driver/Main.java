de3fef1175d976cf13566d344b012cac0275102f
Rohan Vidhate <49265832+roneya@users.noreply.github.com>
2023-01-16 00:10:44 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,8 +1,9 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
    RWOnly obj = new RWOnly();
         obj.setName("Rohan");
         obj.getName();
-  
+    }  
 }
